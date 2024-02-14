e77fc1f7e4409883107e89e72090f399c7876752
test <dankom2007@gmail.com>
2024-02-14 16:13:29 +0300

    1.1

--- app/schemas/schemas_for_items.py
+++ app/schemas/schemas_for_items.py
@@ -2,6 +2,14 @@ from pydantic import BaseModel, UUID4
 
 
 class ItemsFull(BaseModel):
+    title: str
+    price: float
+    picture: str
+    specifications: str
+    user_id: int
+
+
+class ItemsOut(BaseModel):
     id: UUID4
     title: str
     price: float
@@ -10,9 +18,8 @@ class ItemsFull(BaseModel):
     user_id: int
 
 
-class ItemOut(BaseModel):
+class ItemsIn(BaseModel):
     title: str
     price: float
     picture: str
     specifications: str
-    user_id: int
